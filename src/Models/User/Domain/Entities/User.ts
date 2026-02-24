6273a0a3136bbc2c9deeb3e4c29cda0c1f7fdd2e
josesalcedo-hub <jose.salcedo@invercolombia.com.co>
2026-02-24 10:08:26 -0500

    Model user on Prisma

--- src/Models/User/Domain/Entities/User.ts
+++ src/Models/User/Domain/Entities/User.ts
@@ -4,12 +4,14 @@ export class User{
     lastName: string;
     email:string;
     password:string;
+    phone: string;
 
-    constructor(id:number, name:string, lastName: string, email:string, password:string){
+    constructor(id:number, name:string, lastName: string, email:string, password:string, phone:string){
         this.id = id;
         this.name = name;
         this.lastName = lastName;
         this.email = email;
         this.password = password
+        this.phone = phone
     }
 }
